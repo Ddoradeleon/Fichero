abe374a6a497a8e9be7f4ec1d802b1f20414ac55
Ddoradeleon <139251634+Ddoradeleon@users.noreply.github.com>
2023-07-25 15:25:37 -0600

    Add files via upload

---
+++ tarea fichero.cpp
@@ -0,0 +1,13 @@
+//Dora Maribel de Leon ayala
+//cuarto Bach. En computacion
+//tarea de ficheros
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main (){
+string a;
+ofstream fich("ficha.txt");
+cout<<"ingrese una frase:";
+getline(cin,a);
+return 0;
+}
